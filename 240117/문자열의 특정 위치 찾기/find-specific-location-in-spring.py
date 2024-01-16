e7423abcf12b023b5baed6a03092197b531f8ad9
Obi <103497527+OhDG@users.noreply.github.com>
2024-01-17 00:03:22 +0900

    Title: 문자열의 특정 위치 찾기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240117/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
+++ 240117/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
@@ -1,6 +1,10 @@
 n = input().split()
-
+satisfied = False
 for i in range(len(n[0])):
     if n[0][i] == n[1]:
         print(i)
+        satisfied = True
         break
+
+if not satisfied:
+    print("No")
